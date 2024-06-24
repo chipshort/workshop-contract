53fa43d09f3e58b8e8c1cd5a1ccabb6357e89104
Christoph Otter <chris@confio.gmbh>
2024-06-24 16:21:23 +0200

    Add message types and state

--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,38 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Coin;
+
+use crate::state::Metadata;
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub registration_fee: Coin,
+    pub transfer_fee: Coin,
+}
 
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    Register { name: String, metadata: Metadata },
+    Transfer { name: String, to: String },
+    Change { name: String, metadata: Metadata },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    #[returns(crate::state::Entry)]
+    Lookup { name: String },
+
+    #[returns(EntryListResponse)]
+    List {
+        start_after: Option<String>,
+        limit: Option<u32>,
+    },
+
+    #[returns(cosmwasm_std::ValidatorResponse)]
+    QueryValidatorInfo { name: String },
+}
+
+#[cw_serde]
+struct EntryListResponse {
+    entries: Vec<crate::state::Entry>,
+}
--- src/state.rs
+++ src/state.rs
@@ -1,1 +1,25 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::{Addr, Coin};
+use cw_storage_plus::{Item, Map};
 
+#[cw_serde]
+pub struct Config {
+    pub registration_fee: Coin,
+    pub transfer_fee: Coin,
+}
+
+#[cw_serde]
+pub struct Entry {
+    pub owner: Addr,
+    pub metadata: Metadata,
+}
+
+#[cw_serde]
+pub struct Metadata {
+    pub url: Option<String>,
+    pub validator_address: Option<String>,
+    // ...
+}
+
+pub const ENTRIES: Map<&str, Entry> = Map::new("entries");
+pub const CONFIG: Item<Config> = Item::new("config");
